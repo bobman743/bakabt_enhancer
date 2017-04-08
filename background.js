5b44156e16457cba084879509fe9cf6682537dad
bobman743 <4vm2j1bbl8zl05qp@gmail.com>
2017-04-07 22:46:27 -0700

    Remove cruft leftover from getting the initial thing working

--- background.js
+++ background.js
@@ -1,9 +1,3 @@
-// function handleStartup() {
-//   alert("foo");
-// }
-
-// browser.runtime.onStartup.addListener(handleStartup);
-
 function getUserPageURL() {
   return "https://bakabt.me/user/1983187/bobman743";
 }
@@ -15,25 +9,15 @@ function updateTorrents() {
 
      let torrents = {};
 
-     Array.prototype.forEach.call(
-      userPageDocument.getElementById("active").getElementsByClassName("torrent"),
-      function(element) {
-        torrents[element.dataset.torrentid] = "active";
-      });
-
-      Array.prototype.forEach.call(
-      userPageDocument.getElementById("inactive").getElementsByClassName("torrent"),
-      function(element) {
-        console.log("inactive:");
-        console.log(element);
-        console.log(element.dataset.torrentid);
-        torrents[element.dataset.torrentid] = "inactive";
-        console.log(torrents[element.dataset.torrentid]);
-      });
-
-     console.log(torrents);
-
-     // this is async
+     ["active", "inactive"].forEach(function(status) {
+        Array.prototype.forEach.call(
+          userPageDocument.getElementById(status).getElementsByClassName("torrent"),
+          function(element) {
+            torrents[element.dataset.torrentid] = status;
+          });
+     });
+     
+     // this is async, need to check for errors (?)
      browser.storage.local.set({"torrents": torrents});
   }
   var userPageRequest = new XMLHttpRequest();
@@ -42,33 +26,4 @@ function updateTorrents() {
   userPageRequest.send();
 }
 
-
-// function openMyPage() {
-//   // this is async, should check for errors
-//   //browser.storage.local.set({"fookey": "foovalue"});
-
-//   function reqListener () {
-//     let parser = new DOMParser();
-//     let xmlDoc = parser.parseFromString(oReq.responseText, "text/html");
-//     console.log(xmlDoc);
-//     browser.storage.local.set({"foo": xmlDoc});
-
-//     Array.prototype.forEach.call(temp0.getElementById("active").getElementsByClassName("torrent"), function(element) { console.log(element.dataset.torrentid); });
-
-//   }
-
-//   var oReq = new XMLHttpRequest();
-//   oReq.addEventListener("load", reqListener);
-//   oReq.open("GET", "https://bakabt.me/user/1983187/bobman743");
-//   oReq.send();
-
-//   browser.tabs.create({
-//     "url": "/status.html"
-//   });
-// }
-
-
-/*
-Add openMyPage() as a listener to clicks on the browser action.
-*/
 browser.browserAction.onClicked.addListener(updateTorrents);
--- enhance.js
+++ enhance.js
@@ -1,7 +1,3 @@
-console.log("start of enhance.js");
-
-document.body.style.border = "5px solid red";
-
 //Redo this as a function that takes an element and just returns {"isLinkToTorrentPage": true, "torrentId": xxxx}
 function isLinkToTorrentPage(element) {
   let components = element.pathname.split("/");
@@ -13,17 +9,16 @@ function getTorrentId(element) {
   return element.pathname.split("/")[2];
 }
 
-function foo() {
-  console.log("Injecting...");
+function inject() {
 
   function onGetTorrents(storageData) {
-    console.log("Got torrent list. Attempting to update anchors...");
 
-    //console.log(document.getElementsByTagName("a"));
     Array.prototype.forEach.call(
       document.getElementsByTagName("a"),
       function(element) {
         if (isLinkToTorrentPage(element)) {
+          // This is a dumb way of doing this
+          // Also need to add class idempotently, reloading the plugin will apply this multiple times
           if (storageData.torrents[getTorrentId(element)] == "active") {
             element.className += " enhance-active";
           } else if (storageData.torrents[getTorrentId(element)] == "inactive") {
@@ -31,14 +26,9 @@ function foo() {
           }
         }
       });
-
-    console.log(storageData.torrents);
   }
 
   browser.storage.local.get("torrents").then(onGetTorrents);
 }
 
-foo();
-// console.log(document.getElementsByTagName("a"));
-
-console.log("end of enhance.js");
+inject();
